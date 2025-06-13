004c4079a5b9c0b4d3490b4787aad19d46b01aca
Icaro-dePaula-SOMA <icaro.paula@somasolucoes.com>
2025-06-13 17:14:38 -0300

    feat: linter

---
+++ main.py
@@ -0,0 +1,41 @@
+import  oracledb, math, psycopg2
+from datetime import datetime
+
+
+lista =           [1,
+
+
+2,
+3]
+
+def somar(a
+): 
+    a = datetime.now()
+    psycopg2.connect()   
+    oracledb.connect() 
+    a = math.ceil(3.4)
+    duzento = 200
+    if a > duzento:
+        pass
+
+
+teste="variavio de teste pra saber se ta dando certo o linter"
+class CainhoCarajarassinho():
+    def __init__():
+        pass
+
+
+
+
+
+
+if        (teste == True):
+    pass
+
+
+
+teste =7
+
+somar(1)
+
+
