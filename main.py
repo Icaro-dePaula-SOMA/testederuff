31be63fd720b354110d0c43e0c97202003995d2c
Icaro-dePaula-SOMA <icaro.paula@somasolucoes.com>
2025-06-13 17:15:32 -0300

    feat: linter

--- main.py
+++ main.py
@@ -1,41 +1,33 @@
-import  oracledb, math, psycopg2
 from datetime import datetime
+import math
+import oracledb
+import psycopg2
+
+lista = [1, 2, 3]
 
 
-lista =           [1,
-
-
-2,
-3]
-
-def somar(a
-): 
+def somar(a):
     a = datetime.now()
-    psycopg2.connect()   
-    oracledb.connect() 
+    psycopg2.connect()
+    oracledb.connect()
     a = math.ceil(3.4)
     duzento = 200
     if a > duzento:
         pass
 
 
-teste="variavio de teste pra saber se ta dando certo o linter"
-class CainhoCarajarassinho():
+teste = 'variavio de teste pra saber se ta dando certo o linter'
+
+
+class CainhoCarajarassinho:
     def __init__():
         pass
 
 
-
-
-
-
-if        (teste == True):
+if teste:
     pass
 
 
-
-teste =7
+teste = 7
 
 somar(1)
-
-
